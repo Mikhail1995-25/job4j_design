65405187d9dafff3cdf5cc7d82461ce1108880b0
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-06-22 19:10:37 +0300

    2. Не перекрывать equals hashCode [#297002]

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.collection;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -24,4 +27,22 @@ public class User {
     public Calendar getBirthday() {
         return birthday;
     }
+
+    public static void main(String[] args) {
+        User first = new User("Vlad", 2, new GregorianCalendar(1998, 2, 20));
+        User second = new User("Vlad", 2, new GregorianCalendar(1998, 2, 20));
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(first, "Vlad");
+        map.put(second, "Vlad");
+
+        for (User user : map.keySet()) {
+            System.out.println(map);
+        }
+    }
 }
+/**
+ * Происходит вывод целого уникального числа, за счет метода hashcode().
+ * при запуске программы ключи обьектов сравниваются на null,
+ * далее генерерируется hash. Поэтому мы получаем результат в виде целого числа.
+ */
