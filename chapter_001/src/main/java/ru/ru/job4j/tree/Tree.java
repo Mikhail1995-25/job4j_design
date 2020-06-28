869a00e70d956567f080559e72e4fca5dc6bd861
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-06-28 20:24:24 +0300

    1. Создать элементарную структуру дерева [#296987]

---
+++ chapter_001/src/main/java/ru/ru/job4j/tree/Tree.java
@@ -0,0 +1,45 @@
+package ru.ru.job4j.tree;
+
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
+
+public class Tree<E> implements SimpleTree<E> {
+    private final Node<E> root;
+
+    public Tree(final E root) {
+        this.root = new Node<>(root);
+
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean rs1 = false;
+        Optional<Node<E>> par = findBy(parent);
+        if (par.isPresent()) {
+            Optional<Node<E>> ch = findBy(child);
+            if (!ch.isPresent()) {
+                Node node = par.get();
+                node.children.add(new Node(child));
+                rs1 = true;
+            }
+        }
+        return rs1;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rs1 = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rs1 = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rs1;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/TreeTest.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+import org.junit.Test;
+import ru.ru.job4j.tree.Tree;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+public class TreeTest {
+
+    @Test
+    public void when6ElFindLastThen6() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.findBy(6).isPresent(), is(true));
+    }
+
+    @Test
+    public void when6ElFindNotExitThenOptionEmpty() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        assertThat(tree.findBy(7).isPresent(), is(false));
+    }
+}
