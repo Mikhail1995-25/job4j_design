3fc8a80ebd07739b6fff4321ad3faf25fa375a82
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-07-12 13:40:20 +0300

    0.1. FileOutputStream. [#297018]

---
+++ chapter_002/src/main/java/ru/job4j/io/MatrixFile.java
@@ -0,0 +1,17 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class MatrixFile {
+    public static void main(String[] args) {
+       try (FileOutputStream out = new FileOutputStream("matrix.txt")) {
+           for (int i = 1; i <= 10; i++) {
+               for (int j = 1; j <= 10; j++) {
+                   out.write((" " + i + " " + "X" + " " + j + " " + "=" + " " + i * j + "\n").getBytes());
+               }
+           }
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+    }
+}
