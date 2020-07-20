1aab304148395bf1671445c61df50d2da219ec7c
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-07-20 19:46:03 +0300

    4.0. File [#297021]

--- chapter_002/src/main/java/ru/job4j/io/Dir.java
+++ chapter_002/src/main/java/ru/job4j/io/Dir.java
@@ -4,14 +4,14 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\projects");
+        File file = new File("c:\\projects\\job4j_design");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s" + file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
             throw new IllegalArgumentException("Not directory %s" + file.getAbsoluteFile());
         }
-       System.out.println("Имя файла -> " + file.getName());
-        System.out.println("Размер файла -> " + file.length() + " байт");
+      System.out.println(String.format("size : %s", file.getTotalSpace()));
+                System.out.print("Имя файла " + file.getName() + " Размер файла " + file.length() + " байт");
     }
 }
