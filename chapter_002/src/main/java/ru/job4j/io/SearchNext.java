28686f733930cb8e67897131603a3dcc4bd6196b
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-07-23 18:51:37 +0300

    5. Валидация параметров запуска. [#297017]

--- chapter_002/src/main/java/ru/job4j/io/SearchNext.java
+++ chapter_002/src/main/java/ru/job4j/io/SearchNext.java
@@ -8,10 +8,9 @@ import java.util.List;
 
 public class SearchNext {
     public static void main(String[] args) throws IOException {
-       if (args.length == 0) {
-           throw new IllegalArgumentException("Root folder is null");
-       }
-
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Root folder is null");
+        }
        Path start = Paths.get(args[0]);
         search(start, args[1]).forEach(System.out::println);
     }
