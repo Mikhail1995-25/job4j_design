acc4ef1f6f7a889a0948ee36f7446d20ca021eca
Mikhail1995-25 <milhail_pushkarev_1995@mail.ru>
2020-07-28 05:53:42 +0300

    1. Log4j. Логирование системы. [#297010]

---
+++ chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class UsageLog4j {
+
+    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
+        LOG.trace("trace massage");
+        LOG.debug("debug massage");
+        LOG.info("info massage");
+        LOG.warn("warn massage");
+        LOG.error("error massage");
+    }
+}
+
